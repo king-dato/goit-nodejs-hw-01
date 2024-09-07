0dd894f5d29c81779cf9ce563bdf08a373fdc749
king-dato <king_dato@yahoo.com>
2024-09-07 04:39:49 -0400

    Update homework

--- index.js
+++ index.js
@@ -1,47 +1,79 @@
-// const { Command } = require("commander");
-import { Command } from "commander";
-const program = new Command();
+//connect the contacts
+import {
+  getContactById,
+  listContacts,
+  removeContact,
+  addContact,
+} from "./contacts.js";
 
-// prettier-ignore
-import { getContactById, listContacts, removeContact, addContact } from "./contacts.js";
+const argv = require("yargs").argv;
+const contacts = require("./contacts");
 
-program
-  .option("-a, --action <type>")
-  .option("-i, --id <type>")
-  .option("-n, --name <type>")
-  .option("-e, --email <type>")
-  .option("-p, --phone <type>")
-  .allowUnknownOption(true);
-
-program.parse();
-
-const argv = program.opts();
-
-const invokeAction = async ({ action, id, name, email, phone }) => {
+function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      const contacts = await listContacts();
-      console.table(contacts);
+      contacts2.listContacts().then(console.table);
       break;
 
     case "get":
-      const contactsById = await getContactById(id);
-      console.log(contactsById);
+      contacts2.getContactById(id).then(console.log);
       break;
 
     case "add":
-      const addNewContact = await addContact({ name, email, phone });
-      console.log(addNewContact);
+      contacts2
+        .addContact(name, email, phone)
+        .then(() => console.log("Contact added"));
       break;
 
     case "remove":
-      const deletedContact = await removeContact(id);
-      console.log(deletedContact);
+      contacts2.removeContact(id).then(() => console.log("Contact removed"));
       break;
 
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
-};
+}
+
+invokeAction(argv);
+//using commander
+
+const { Command } = require("commander");
+const program = new Command();
+const contacts3 = require("./contacts");
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "contact id")
+  .option("-n, --name <type>", "contact name")
+  .option("-e, --email <type>", "contact email")
+  .option("-p, --phone <type>", "contact phone");
+
+program.parse(process.argv);
+const argv2 = program.opts();
+
+function invokeAction2({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      contacts3.listContacts().then(console.table);
+      break;
+
+    case "get":
+      contacts3.getContactById(id).then(console.log);
+      break;
+
+    case "add":
+      contacts3
+        .addContact(name, email, phone)
+        .then(() => console.log("Contact added"));
+      break;
+
+    case "remove":
+      contacts3.removeContact(id).then(() => console.log("Contact removed"));
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
 
 invokeAction(argv);
