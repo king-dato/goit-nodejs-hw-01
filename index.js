b3176b33419c719934cda044da0078ad3890ed35
king-dato <king_dato@yahoo.com>
2024-09-07 11:13:06 -0400

    Update JAva

--- index.js
+++ index.js
@@ -47,71 +47,39 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
 
 invokeAction(argv);
 
-// function invokeAction({ action, id, name, email, phone }) {
-//   switch (action) {
-//     case "list":
-//       contacts2.listContacts().then(console.table);
-//       break;
 
-//     case "get":
-//       contacts2.getContactById(id).then(console.log);
-//       break;
-
-//     case "add":
-//       contacts2
-//         .addContact(name, email, phone)
-//         .then(() => console.log("Contact added"));
-//       break;
-
-//     case "remove":
-//       contacts2.removeContact(id).then(() => console.log("Contact removed"));
-//       break;
-
-//     default:
-//       console.warn("\x1B[31m Unknown action type!");
-//   }
-// }
-
-// invokeAction(argv);
-//using commander
 
 import { Command } from "commander";
 const program = new Command();
-const contacts3 = require("./contacts");
-
 program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "contact id")
-  .option("-n, --name <type>", "contact name")
-  .option("-e, --email <type>", "contact email")
-  .option("-p, --phone <type>", "contact phone");
+  .version('1.0.0')
+  .description('CLI for demo purposes')
+  .option('-a, --action <type>', 'Action to perform')
+  .option('-i, --id <type>', 'ID of the contact')
+  .option('-n, --name <type>', 'Name of the contact')
+  .option('-e, --email <type>', 'Email of the contact')
+  .option('-p, --phone <type>', 'Phone of the contact')
+  .parse(process.argv);
 
-program.parse(process.argv);
-const argv2 = program.opts();
+const options = program.opts();
 
-function invokeAction2({ action, id, name, email, phone }) {
+const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
-    case "list":
-      contacts3.listContacts().then(console.table);
+    case 'list':
+      // Logic to list contacts
       break;
-
-    case "get":
-      contacts3.getContactById(id).then(console.log);
+    case 'get':
+      // Logic to get a contact by ID
       break;
-
-    case "add":
-      contacts3
-        .addContact(name, email, phone)
-        .then(() => console.log("Contact added"));
+    case 'add':
+      // Logic to add a contact
       break;
-
-    case "remove":
-      contacts3.removeContact(id).then(() => console.log("Contact removed"));
+    case 'remove':
+      // Logic to remove a contact
       break;
-
     default:
-      console.warn("\x1B[31m Unknown action type!");
+      console.warn('Unknown action type!');
   }
-}
+};
 
-invokeAction(argv);
+invokeAction(options);
