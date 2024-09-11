4463ca58892828b5c1b2b9fd5efdbcc525f1e8b9
king-dato <king_dato@yahoo.com>
2024-09-11 00:46:36 -0400

    UPdate

--- index.js
+++ index.js
@@ -1,85 +1,47 @@
-//connect the contacts
-import {
-  getContactById,
-  listContacts,
-  removeContact,
-  addContact,
-} from "./contacts.js";
-
-// const argv = require("yargs").argv;
-import yargs from "yargs";
-
-import { hideBin } from "yargs/helpers";
-
-const argv = yargs(hideBin(process.argv))
-  .option("action", {
-    alias: "a",
-    describe: "Action to perform",
-    type: "string",
-  })
-  .option("id", {
-    alias: "i",
-    describe: "ID of the contact",
-    type: "string",
-  })
-  .option("name", {
-    alias: "n",
-    describe: "Name of the contact",
-    type: "string",
-  })
-  .option("email", {
-    alias: "e",
-    describe: "Email of the contact",
-    type: "string",
-  })
-  .option("phone", {
-    alias: "p",
-    describe: "Phone of the contact",
-    type: "string",
-  })
-  .help()
-  .alias("help", "h").argv;
-
-// Your invokeAction function here
-const invokeAction = async ({ action, id, name, email, phone }) => {
-  // Your switch case or logic
-};
-
-invokeAction(argv);
-
-
-
+// const { Command } = require("commander");
 import { Command } from "commander";
 const program = new Command();
-program
-  .version('1.0.0')
-  .description('CLI for demo purposes')
-  .option('-a, --action <type>', 'Action to perform')
-  .option('-i, --id <type>', 'ID of the contact')
-  .option('-n, --name <type>', 'Name of the contact')
-  .option('-e, --email <type>', 'Email of the contact')
-  .option('-p, --phone <type>', 'Phone of the contact')
-  .parse(process.argv);
 
-const options = program.opts();
+// prettier-ignore
+import { getContactById, listContacts, removeContact, addContact } from "./contacts.js";
+
+program
+  .option("-a, --action <type>")
+  .option("-i, --id <type>")
+  .option("-n, --name <type>")
+  .option("-e, --email <type>")
+  .option("-p, --phone <type>")
+  .allowUnknownOption(true);
+
+program.parse();
+
+const argv = program.opts();
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
-    case 'list':
-      // Logic to list contacts
+    case "list":
+      const contacts = await listContacts();
+      console.table(contacts);
       break;
-    case 'get':
-      // Logic to get a contact by ID
+
+    case "get":
+      const contactsById = await getContactById(id);
+      console.log(contactsById);
       break;
-    case 'add':
-      // Logic to add a contact
+
+    case "add":
+      const addNewContact = await addContact({ name, email, phone });
+      console.log(addNewContact);
       break;
-    case 'remove':
-      // Logic to remove a contact
+
+    case "remove":
+      const deletedContact = await removeContact(id);
+      console.log(deletedContact);
       break;
+
     default:
-      console.warn('Unknown action type!');
+      console.warn("\x1B[31m Unknown action type!");
   }
 };
 
-invokeAction(options);
+invokeAction(argv);
